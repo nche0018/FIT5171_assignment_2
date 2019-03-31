5e46270aa1b1712dfd6680633cef636421b8c785
Yan <yan.emma.liu@gmail.com>
2019-03-31 21:10:45 +1100

    [updated miner methods]

--- src/main/java/rockets/mining/RocketMiner.java
+++ src/main/java/rockets/mining/RocketMiner.java
@@ -24,7 +24,7 @@ public class RocketMiner {
 
     /**
      * TODO: to be implemented & tested!
-     * Returns the top-k active rocket, as measured by number of launches.
+     * Returns the top-k most active rockets, as measured by number of completed launches.
      *
      * @param k the number of rockets to be returned.
      * @return the list of k most active rockets.
@@ -63,15 +63,12 @@ public class RocketMiner {
     /**
      * TODO: to be implemented & tested!
      * <p>
-     * Returns the successful launch rate in <code>year</code> measured by the
-     * number of successful launches and total number of launches
+     * Returns the dominant country who has the most launched rockets in an orbit.
      *
-     * @param year the year
-     * @return the successful launch rate in BigDecimal with scale 2.
+     * @param orbit the orbit
+     * @return the country who sends the most payload to the orbit
      */
-    public BigDecimal successfulLaunchRateInYear(int year) {
-        return BigDecimal.valueOf(0);
-    }
+    public String dominantCountry(String orbit) { return null;}
 
     /**
      * TODO: to be implemented & tested!
@@ -82,6 +79,20 @@ public class RocketMiner {
      * @return the list of k most expensive launches.
      */
     public List<Launch> mostExpensiveLaunches(int k) {
+        return null;
+    }
+
+    /**
+     * TODO: to be implemented & tested!
+     * <p>
+     * Returns a list of launch service provider that has the top-k highest
+     * sales revenue in a year.
+     *
+     * @param k the number of launch service provider.
+     * @param year the year in request
+     * @return the list of k launch service providers who has the highest sales revenue.
+     */
+    public List<LaunchServiceProvider> highestRevenueLaunchServiceProviders(int k, int year) {
         return null;
     }
 }
